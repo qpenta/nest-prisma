d1769418cc8f2ffe48559023d69759c89226e124
qpenta <zzyagos1@gmail.com>
2023-05-27 14:55:36 -0300

    resolução de erros

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,12 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Delete } from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDTO } from './bookdto';
 
 @Controller('book')
 export class BookController {
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService) { }
 
   @Post()
   async create(@Body() data: BookDTO) {
     return this.bookService.create(data);
+  }
+
+  @Delete()
+  async delete(@Body() data: string) {
+    return this.bookService.delete(data);
   }
 }
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -4,25 +4,42 @@ import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
 export class BookService {
-
-    const bookExists = await this.prisma.book.findFirst({
-        where: {
-            bar_code: data.bar_code,
-        }
-    })
-
-    if(bookExists) {
-        throw new Error('Book already exists');
-    }
-
     constructor(private prisma: PrismaService) { }
 
-    async create(data: BookDTO) {
-        const book = await this.prisma.book.create({
-            data,
-        });
+    async create(bookData: BookDTO) {
+        const bookExists = await this.prisma.book.findFirst({
+            where: {
+                bar_code: bookData.bar_code,
+            }
+        })
 
-        return book;
+        if (bookExists) {
+            throw new Error('Book already exists');
+        }
+
+        const book = await this.prisma.book.create({
+            data: bookData,
+        })
+
+        return book
+    }
+
+    async delete(data: string) {
+        const barCodeExists = await this.prisma.book.findUnique({
+            where: {
+                bar_code: data["bar_code"]
+            }
+        })
+
+        if (!barCodeExists) {
+            throw new Error('This barcode does not exist')
+        }
+
+        return await this.prisma.book.delete({
+            where: {
+                bar_code: data["bar_code"],
+            }
+        })
     }
 
 }
